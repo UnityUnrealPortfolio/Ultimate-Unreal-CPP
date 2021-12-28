04c3a91a4754341b39cc6768a1fa199e5507828a
Auravision <auravision.africa@gmail.com>
2021-12-28 10:56:20 +0300

    Moving on to Character Dev

---
+++ Source/FirstProject_SU/GameplayActors/CritterMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "CritterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_SU_API UCritterMovementComponent : public UPawnMovementComponent
+{
+	GENERATED_BODY()
+	
+};
